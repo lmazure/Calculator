21025a20598722908eda4ce08e43a4fb9d30d98a
Laurent Mazuré <lmazure@henix.fr>
2021-05-26 19:59:48 +0200

    Added Subtracter.

---
+++ src/main/java/computer/Subtracter.java
@@ -0,0 +1,18 @@
+package computer;
+
+public class Subtracter extends BinaryOperator {
+
+	public Subtracter(final Operand o1, final Operand o2) {
+		super(o1, o2);
+	}
+
+	@Override
+	public double getValue() {
+		return o1.getValue() - o2.getValue();
+	}
+
+	@Override
+	public String getDescription() {
+		return "(" + o1.getDescription() + ") - (" + o2.getDescription() + ")";
+	}
+}
---
+++ src/test/java/computer/SubtracterTest.java
@@ -0,0 +1,36 @@
+package computer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SubtracterTest {
+
+    @Test
+    void checkValue() {
+
+        // -- arrange
+        final Number n1 = new Number(1.3d, "n1");
+        final Number n2 = new Number(1.7d, "n2");
+        
+        // -- act
+        final Operand effective = new Subtracter(n1, n2);
+
+        // -- assert
+        assertEquals(-0.4d, effective.getValue(), 0.0001d);
+    }
+
+    @Test
+    void checkDescription() {
+
+        // -- arrange
+        final Number n1 = new Number(1.3d, "n1");
+        final Number n2 = new Number(1.7d, "n2");
+        
+        // -- act
+        final Operand effective = new Subtracter(n1, n2);
+
+        // -- assert
+        assertEquals("(n1) - (n2)", effective.getDescription());
+    }
+}
