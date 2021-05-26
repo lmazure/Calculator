e02d168fa103c898a9a463afe1f26411ac10ea4c
Laurent Mazuré <lmazure@henix.fr>
2021-05-26 20:06:17 +0200

    Added Divider.

---
+++ src/main/java/computer/Divider.java
@@ -0,0 +1,18 @@
+package computer;
+
+public class Divider extends BinaryOperator {
+
+	public Divider(final Operand o1, final Operand o2) {
+		super(o1, o2);
+	}
+
+	@Override
+	public double getValue() {
+		return o1.getValue() / o2.getValue();
+	}
+
+	@Override
+	public String getDescription() {
+		return "(" + o1.getDescription() + ") / (" + o2.getDescription() + ")";
+	}
+}
---
+++ src/test/java/computer/DividerTest.java
@@ -0,0 +1,37 @@
+package computer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DividerTest {
+
+    @Test
+    void checkValue() {
+
+        // -- arrange
+        final Number n1 = new Number(2.0d, "n1");
+        final Number n2 = new Number(0.3d, "n2");
+        
+        // -- act
+        final Operand effective = new Divider(n1, n2);
+
+        // -- assert
+        assertEquals(6.66666666666666666667d, effective.getValue(), 0.0001d);
+    }
+
+    @Test
+    void checkDescription() {
+
+        // -- arrange
+        final Number n1 = new Number(1.3d, "n1");
+        final Number n2 = new Number(1.7d, "n2");
+        
+        // -- act
+        final Operand effective = new Divider(n1, n2);
+
+        // -- assert
+        assertEquals("(n1) / (n2)", effective.getDescription());
+    }
+
+}
