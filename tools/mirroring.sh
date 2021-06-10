d164ea81c27afd53326b990966745ce2b4e3dbea
Laurent Mazuré <lmazure@henix.fr>
2021-06-10 08:29:57 +0200

    fixed variable name

--- tools/mirroring.sh
+++ tools/mirroring.sh
@@ -9,4 +9,4 @@ cd $source_project.git
 git show-ref | grep ' refs/merge-requests/' | cut -d' ' -f2 | xargs -n1 -r git update-ref -d # do not push merge-requests refs to GitLab, it will refuse them
 git push --mirror https://$target_repo/$target_project.git
 cd ..
-rm -fr $project.git
+rm -fr $source_project.git
